f80c402d6a2c6a3ef2867db9f7b7f20922553c24
nodeloc <james@nodeloc.com>
2026-02-08 00:22:49 +0800

    add tips

--- frontend/src/views/PurchaseView.vue
+++ frontend/src/views/PurchaseView.vue
@@ -67,6 +67,12 @@
             <span>{{ error }}</span>
           </div>
           
+          <!-- Pre-Purchase Notice -->
+          <div class="alert alert-info mt-4">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+            <span class="text-sm">{{ $t('purchase.notice') }}</span>
+          </div>
+          
           <div class="card-actions justify-end mt-4">
             <button @click="processPurchase" class="btn btn-primary" :disabled="!paymentMethod || processing">
               <span v-if="processing" class="loading loading-spinner loading-sm mr-2"></span>
